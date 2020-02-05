0f74e73f804ab5a638d020e5007c3f2f14f2a173
lalihul <ilyalali8reler@gmail.com>
2020-02-05 03:09:14 +0300

    fixed a small bug

--- test1.py
+++ test1.py
@@ -31,7 +31,7 @@ def ask_yes_no(question):
 def ask_number(question, low, high):
     #asks you to enter a number from the range
     response = None 
-    while response not in range(0, 9):
+    while response not in range(low, high):
         response = int(input(question))
     return response
 
